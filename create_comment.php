7921a7cb81fb082064869044b4f6ca21e09796d0
Zedw1n <alexs.tkach@mail.ru>
2023-07-18 17:23:08 +0700

    убрал возможность добавления html тегов в коментарий

--- create_comment.php
+++ create_comment.php
@@ -1,11 +1,11 @@
 <?php 
 session_start();
 require_once('db_connection.php');
-
-$name = $_POST['name'];  $name = stripcslashes($name);  $name = mysqli_real_escape_string($connection, $name);  $name = preg_replace('/[^\p{L}\p{N}\s]/u', '', $name);
-$comment = $_POST['comment'];  $comment = stripcslashes($comment); $comment = mysqli_real_escape_string($connection, $comment);  $comment = preg_replace('/[^\p{L}\p{N}\s]/u', '', $comment);
-
-mysqli_query($connection, query:"INSERT INTO `comments` (`comment_id`, `name`, `comment`,`date`) VALUES (NULL, '$name', '$comment',now())");
+$PreparedQuery = $connection -> prepare("INSERT INTO `comments` (`comment_id`, `name`, `comment`,`date`) VALUES (NULL, ?, ?, now())");
+$name = $_POST['name']; $name = strip_tags($name);
+$comment = $_POST['comment']; $comment = strip_tags($comment);
+$PreparedQuery -> bind_param('ss', $name, $comment);
+$PreparedQuery -> execute();
 $_SESSION['commented'] = true;
 header('Location: /');
 ?>
