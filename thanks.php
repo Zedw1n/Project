2628777860b9150b74e5140c062293a10f2b5b44
Zedw1n <alexs.tkach@mail.ru>
2023-07-21 14:11:22 +0700

    изменил хедер слайдер для разрешений меньше 1920 как по макету

--- thanks.php
+++ thanks.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <title>Stationeries</title>
-    <link rel="stylesheet" href="css/main.css">
-    
+    <link rel="stylesheet" href="css/style.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <div class="thanks_container">
