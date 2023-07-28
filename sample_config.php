967e9ba2e87aae76b581226405bc7a26e0b4f1cf
Zedw1n <alexs.tkach@mail.ru>
2023-07-28 19:11:36 +0700

    загрузил файл шаблона конфига

---
+++ sample_config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+    "production" => true,
+    "hostname" => 'localhost',
+    "login" => 'Логин базы данных',
+    "password" => 'Пароль',
+    "database" => 'Имя базы данных',
+    "encoding" => 'utf8'
+    )
+?>
